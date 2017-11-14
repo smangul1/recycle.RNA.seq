04efa3e01982b468cb85e7dfba8d32f8dab7b43c
Serghei Mangul <smangul@ucla.edu>
2017-11-14 03:57:32 -0800

    add figures

--- figures/mapped_reads_simulated.R
+++ figures/mapped_reads_simulated.R
@@ -7,7 +7,7 @@ data.m <- melt(data, id.vars='cat')
 
 pdf("mapped_reads_simulated.pdf",width=6,height=4) 
 
-ggplot(data.m, aes(cat, value))+geom_bar(aes(fill = variable), position = "dodge", stat="identity")+scale_fill_manual(values=c("#999999", "#E69F00", "#56B4E9","black","red"))+theme_minimal()+labs(y = "Number of categorized  reads (%)")+labs(x = "Read type")
+ggplot(data.m, aes(cat, value*100))+geom_bar(aes(fill = variable), position = "dodge", stat="identity")+scale_fill_manual(values=c("#999999", "#E69F00", "#56B4E9","black","red"))+theme_minimal()+labs(y = "Number of mapped  reads (%)")+labs(x = "Read type")
 
 dev.off()
 
