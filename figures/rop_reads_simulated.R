53d2630eae336b067009b28a20bab3b2c77cd1de
Serghei Mangul <serghei@login2.(none)>
2017-11-26 19:04:32 -0800

    fix numbers in rop_reads_simulated.csv

--- figures/rop_reads_simulated.R
+++ figures/rop_reads_simulated.R
@@ -5,7 +5,7 @@ library(reshape2)
 data=read.csv("rop_reads_simulated.csv")
 data.m <- melt(data, id.vars='cat')
 
-pdf("rop_reads_simulated.pdf",width=7,height=4)
+pdf("rop_reads_simulated.pdf",width=10,height=4)
 
 ggplot(data.m, aes(cat, value*100))+geom_bar(aes(fill = variable), position = "dodge", stat="identity")+scale_fill_manual(values=c("#999999", "#E69F00", "#56B4E9","black","red","chocolate","deeppink","darkmagenta"))+theme_minimal()+labs(y = "Number of categorized  reads (%)")+labs(x = "Read type")
 
