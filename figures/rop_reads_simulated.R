1c38212ef8a96a8a0e2453ff4f5362d98abefd57
Serghei Mangul <serghei@login4.(none)>
2017-11-26 18:05:59 -0800

    update colors

--- figures/rop_reads_simulated.R
+++ figures/rop_reads_simulated.R
@@ -7,7 +7,7 @@ data.m <- melt(data, id.vars='cat')
 
 pdf("rop_reads_simulated.pdf",width=7,height=4)
 
-ggplot(data.m, aes(cat, value*100))+geom_bar(aes(fill = variable), position = "dodge", stat="identity")+scale_fill_manual(values=c("#999999", "#E69F00", "#56B4E9","black","red","chocolate","darkgray","darkmagenta"))+theme_minimal()+labs(y = "Number of categorized  reads (%)")+labs(x = "Read type")
+ggplot(data.m, aes(cat, value*100))+geom_bar(aes(fill = variable), position = "dodge", stat="identity")+scale_fill_manual(values=c("#999999", "#E69F00", "#56B4E9","black","red","chocolate","deeppink","darkmagenta"))+theme_minimal()+labs(y = "Number of categorized  reads (%)")+labs(x = "Read type")
 
 #ggplot(data.m, aes(cat, value*100))+geom_bar(aes(fill = variable), position = "dodge", stat="identity")+scale_fill_manual(values=c("#999999", "#E69F00", "#56B4E9","black","red","chocolate","darkgray","darkmagenta"))+theme_minimal()+labs(y = "Number of mapped  reads (%)")+labs(x = "Read type")
 
