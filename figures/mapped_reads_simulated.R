7ecf99af4922bdf39cc6daaa2d74798c61fd3b19
Serghei Mangul <serghei@login4.(none)>
2017-11-26 17:39:33 -0800

    add results for RNA-Seq simulated tuned

--- figures/mapped_reads_simulated.R
+++ figures/mapped_reads_simulated.R
@@ -5,7 +5,7 @@ library(reshape2)
 data=read.csv("mapped_reads_simulated.csv")
 data.m <- melt(data, id.vars='cat')
 
-pdf("mapped_reads_simulated.pdf",width=6,height=4) 
+pdf("mapped_reads_simulated.pdf",width=10,height=4) 
 
 ggplot(data.m, aes(cat, value*100))+geom_bar(aes(fill = variable), position = "dodge", stat="identity")+scale_fill_manual(values=c("#999999", "#E69F00", "#56B4E9","black","red","chocolate","darkgray","darkmagenta"))+theme_minimal()+labs(y = "Number of mapped  reads (%)")+labs(x = "Read type")
 
